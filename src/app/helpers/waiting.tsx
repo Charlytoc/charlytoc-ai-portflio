057c1ef10e9c873a337ae071035db4610ea7414b
Charlytoc <charlyjchaconc@gmail.com>
2023-04-02 19:48:08 -0500

    corner

--- src/app/helpers/waiting.tsx
+++ src/app/helpers/waiting.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 export const waiting = {
     greeting: 'Hello, ',
     currentlyWorking: `currently I'm working in this page, it will be available soon!`,
-    byTheWay: `while I'm working you can check the links in the menu`,
+    byTheWay: `while I'm working you can talk to my AI in the right bottom corner!`,
     linkedin: <Link className="button-outline-mint" href={`https://www.linkedin.com/in/charlytoc/`} >Linkedin</Link>,
     github: <Link className="button-outline-mint" href={`https://github.com/Charlytoc`} >Github</Link>,
     portofolio: <Link className="button-outline-mint" href={`https://charlytoc-portfolio.vercel.app/`} >Last portfolio</Link>,
